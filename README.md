60f5a6fbf78f7c3eb1bef1d9a30305f786501281
Daniel Cloud <daniel@danielcloud.org>
2016-03-21 22:36:56 -0400

    Add starte code and instructions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # basic-javascript-starter
-Starter for making basic javascript
+
+We'll explore JavaScript by changing the file `program.js` and reloading the `index.html` in our browser to rerun the script and see the result. `program.js` is already set to load in `index.html` through use of the `<script>` element.
+
+`program.js` is a file we store JavaScript code in, as indicated by the `.js` file extension. The filename extension tells us (and the web browser) to expect JavaScript code.
+
+In `program.js`, I've already created a variable I named *message*. I indicated I wanted to create a variable with the name *message* by first typing the keyword *var* followed by a space, then the word *message* followed by a semicolon. Those three elements (the *var* keyword, my chosen name *message* and the semicolon) were all needed to declare my variable.
+
+You'll see sentences between `/*` and `*/`, which may appear as grey text in a code editor. This is a comment area (marked by the opening `/*` and closing `*/`), allowing us to keep notes along with our code.
+
+I have placed instructions in these comment areas. Read the comments in `program.js` and alter the code as advised to try out different JavaScript expressions.
---
+++ js/program.js
@@ -0,0 +1,39 @@
+/*
+  We'll declare a variable named "message" to store
+  information we will display to the screen.
+  We can name variables whatever we want, but it's helpful to use meaningful names.
+*/
+var message;
+
+/*
+  Here we can assign a value to our variable. Try changing the value on the
+  right side of the = and reloading the page in your browser.
+  For the following, change the value of message by editing
+  the part after the = and before the ;. Make an edit and reload the browser to
+  see the result.
+  Change message to:
+    - a number
+    - a true or false boolean
+
+  Next, try combining one or more values using the + symbol:
+    - The words "hello" and "world" (dont forget to put quotation marks around the words)
+    - The string/sentence 'The number of the day is ' with a number
+
+  Once you have the hang of editing the 'message' variable, try
+  declaring a new variable, with a name you pick. Declare your new variable and
+  assign a value, your name, to it (don't forget quotation marks). Change 'message'
+  to store "Hello " plus the new variable. This should look like
+  arithmetic addition between a word in qutation marks and a variable. See if you can
+  get the browser to display a custom "Hello" message to you.
+*/
+
+message = "Hello";
+
+/*
+  Below we take advantage of the fact that the web browser provides
+  a 'document' variable we can use to manipulate the page. The `write()` part is
+  a function that is part of the 'document' variable. The code below is telling
+  the browser to write information into the HTML document.
+*/
+
+document.write(message);
